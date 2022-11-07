7d8089a78af400712c496775e7f069bb94ac5029
jamesrave <117628362+jamesrave@users.noreply.github.com>
2022-11-07 10:54:06 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,12 @@
+﻿program kr1;
+var x,y:real;
+begin
+  readln(x);
+  if x<-8 then
+  y:=tan(x)*ln(x)
+  else if x<=-4 then
+  y:=cos(2*x)-ln(x)
+  else if x>=-4 then
+  y:=(ln(x)/x*x*x)*(cos(2*x)/x*x);
+  writeln(y:0:2);
+end.
---
+++ 2.pas
@@ -0,0 +1,17 @@
+﻿program kr2;
+var x,y,z,h:real;
+begin
+  x:=-10;
+  h:=0.2;
+  while x<=-2 do
+  begin
+    if x<-8 then
+  y:=tan(x)*ln(x)
+  else if x<=-4 then
+  y:=cos(2*x)-ln(x)
+  else if x>=-4 then
+  y:=(ln(x)/x*x*x)*(cos(2*x)/x*x);
+  writeln('x=',x:0:2,' y=',y:0:2);
+  x:=x+h;
+  end;
+end.
